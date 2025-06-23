498a3e401f13eab658d50e671557c230dd9e843c
taruchin0 <callmextarnx@gmail.com>
2025-06-23 16:16:01 +0000

    cell02

--- cell02/ex00/iszero.py
+++ cell02/ex00/iszero.py
@@ -2,6 +2,6 @@
 number = int(input("Enter a number: "))
 
 if number == "0":
-    print("This number is equal to zero.")
+    print("This number is equal to zero")
 else:
     print("This number is different from zero.")
