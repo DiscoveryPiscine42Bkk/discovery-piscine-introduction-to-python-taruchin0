de8ace968582ac5428e3db2d47c9080e068ea3b0
taruchin0 <callmextarnx@gmail.com>
2025-06-23 12:13:39 +0000

    ex03

--- cell02/ex00/iszero.py
+++ cell02/ex00/iszero.py
@@ -2,6 +2,6 @@
 number = input("Enter a number: ").strip()
 
 if number == "0":
-    print("The number is equal zero.")
+    print("This number is equal to zero.")
 else:
-    print("The number is different from zero.")
+    print("This number is different from zero.")
