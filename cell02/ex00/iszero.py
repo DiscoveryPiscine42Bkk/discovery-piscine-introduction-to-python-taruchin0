3ffeb4588637dc8096ecb8fce574998b900900fd
taruchin0 <callmextarnx@gmail.com>
2025-06-23 11:56:55 +0000

    cell02

---
+++ cell02/ex00/iszero.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python
+number = input("Enter a number: ").strip()
+
+if number == "0":
+    print("The number is equal zero.")
+else:
+    print("The number is different from zero.")
