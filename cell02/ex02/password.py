75977496f43af121c9bd2eeca4a7f75c8cdcc86a
taruchin0 <callmextarnx@gmail.com>
2025-06-23 12:39:09 +0000

    ex02

---
+++ cell02/ex02/password.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python
+password = "Python is awesome"
+password = input("Enter a password: ").strip()
+if password == "Python is awesome":
+    print("ACCESS GRANTED")
+else:
+    print("ACCESS DENIED")
