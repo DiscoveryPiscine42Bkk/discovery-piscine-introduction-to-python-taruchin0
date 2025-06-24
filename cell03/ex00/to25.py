c28a3d15e46fe0c071ced893c2334b76d864c767
taruchin0 <callmextarnx@gmail.com>
2025-06-24 03:25:59 +0000

    cell03 ex00

---
+++ cell03/ex00/to25.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+number = int(input("Enter a number less than 25: "))
+
+if number > 25:
+    print("Error")
+else:
+    while(number < 26):
+        print("Inside the loop, my variable is " , number)
+        number += 1
