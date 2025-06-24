3e934549558daa20a0526ae3ff482536af5142e9
taruchin0 <callmextarnx@gmail.com>
2025-06-24 12:22:30 +0000

    cell05 ex02

--- cell05/ex01/play_with_arrays.py
+++ cell05/ex01/play_with_arrays.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python
 
-Original_array = [13, 45, 23, 87, 2]
-New_array = []
-for number in Original_array:
-    New_array.append(number + 2)
-print("Original_array: ", Original_array)
-print("New_array: ", New_array)
+Original_arrays = [13, 45, 23, 87, 2]
+New_arrays = []
+for number in Original_arrays:
+    New_arrays.append(number + 2)
+print("Original_arrays: ", Original_arrays)
+print("New_arrays: ", New_arrays)
---
+++ cell05/ex02/play_with_arrays.py
@@ -0,0 +1,5 @@
+Orinal_arrays = [2, 8, 9, 48, 8, 22, -12, 2]
+for number in Orinal_arrays:
+    if number > 5:
+        print(number, end=' ')
+print()
