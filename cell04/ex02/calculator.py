9bb16b9064e8ddf611bf8b36c2ae44e8785b1f09
taruchin0 <callmextarnx@gmail.com>
2025-06-24 04:52:35 +0000

    cell04 ex02

---
+++ cell04/ex02/calculator.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+first_number = int(input("Give me the first number: "))
+second_number = int(input("Give me the second number: "))
+
+x = first_number
+y = second_number
+print("x", "+", "y", "=", x + y)
+print("x", "-", "y", "=", x - y)
+print("x", "/", "y", "=", x / y)
+print("x", "*", "y", "=", x * y)
