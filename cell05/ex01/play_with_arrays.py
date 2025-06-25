8d42a96b0abde189df744596fae2fb4e0dd8be1c
taruchin0 <callmextarnx@gmail.com>
2025-06-25 13:12:29 +0000

    cell05

--- cell05/ex01/play_with_arrays.py
+++ cell05/ex01/play_with_arrays.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 
-Original_arrays = [-11, 45, 23, -87, 2, 30]
+Original_arrays = [2 , 8 , 9 , 48 , 8 , 22 , -12 , 2]
 New_arrays = []
 for number in Original_arrays:
     New_arrays.append(number + 2)
--- cell05/ex03/play_with_arrays.py
+++ cell05/ex03/play_with_arrays.py
@@ -1,16 +1,9 @@
 #!/usr/bin/env python
 
-Original_arrays = [-11, 45, 45, 23, 23, -87, 2, 30]
-New_arrays = [number + 2 for number in Original_arrays]
-
+Original_arrays = [2 , 8 , 9 , 48 , 8 , 22 , -12 , 2]
 New_arrays = set()
-Duplicate = set()
-
-for number in New_arrays:
-    if number in New_arrays:
-        Duplicate.add(number)
-    else:
-        New_arrays.add(number)
-result = [number for number in New_arrays if number not in Duplicate and number > 5]
-for number in result:
-    print(number, end=' ')
+for number in Original_arrays:
+    if number > 5:
+        New_arrays.add(number+2)
+print(Original_arrays)
+print(New_arrays)
