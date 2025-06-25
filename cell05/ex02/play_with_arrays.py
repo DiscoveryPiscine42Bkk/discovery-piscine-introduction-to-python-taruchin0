f8cd53a10df0ff2cc3674b1078c8963092a890de
taruchin0 <callmextarnx@gmail.com>
2025-06-25 13:26:15 +0000

    cell05

--- cell05/ex02/play_with_arrays.py
+++ cell05/ex02/play_with_arrays.py
@@ -3,8 +3,7 @@
 Original_arrays = [2 , 8 , 9 , 48 , 8 , 22 , -12 , 2]
 New_arrays = []
 for number in Original_arrays:
-    New_arrays.append(number + 2)
-for number in New_arrays:
     if number > 5:
-        print(number, end=' ')
-print()
+        New_arrays.append(number+2)
+print(Original_arrays)
+print(New_arrays)
